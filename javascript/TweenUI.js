48181c1f6b2dbe5f0ebe77d0730242533c4eadf8
Lou Jiwei <jiwei.lou@foxmail.com>
2015-11-11 17:19:29 +0800

    样式修改

--- javascript/TweenUI.js
+++ javascript/TweenUI.js
@@ -12,19 +12,19 @@ timeline.insert(new TweenLite({}, 0.0, {}), 8.0);
 // timeline.insert(TweenLite.to(mylayer_1, 2,{css: {top:200,left:0},ease: Bounce.easeOut}),0.5);
 // TweenLite.set(mylayer_1, {css: {autoAlpha: 1.0, left: 0, top: 0, scale: 1.0, rotation: 0.0}});
 
-var layer_1 = document.getElementById("tui-9");
-timeline.insert(TweenLite.to(layer_1, 0.74, {css: {top: 305, left: 382}, ease: Bounce.easeOut}), 3.6);//第二个时间是等待时间
-timeline.insert(TweenLite.to(layer_1, 0.42, {css: {autoAlpha: 1.0}, ease: Power1.easeOut}), 3.6);
-timeline.insert(TweenLite.to(layer_1, 1.74, {css: {top: -11, left: 23}, ease: Linear.easeNone}), 6.1);//移动
-timeline.insert(TweenLite.to(layer_1, 1.74, {css: {scale: 0.6}, ease: Linear.easeNone}), 6.1);//变小
-TweenLite.set(layer_1, {css: {autoAlpha: 0.0, left: 382, top: -84, scale: 1.0, rotation: 0.0}});//原本的状态
-var layer_2 = document.getElementById("tui-8");
-timeline.insert(TweenLite.to(layer_2, 0.72, {css: {top: 283, left: 643}, ease: Power4.easeOut}), 1.2);
-timeline.insert(TweenLite.to(layer_2, 0.36, {css: {autoAlpha: 1.0}, ease: Power1.easeOut}), 1.2);
-timeline.insert(TweenLite.to(layer_2, 0.44, {css: {scale: 1.84}, ease: Power4.easeOut}), 2.3);
-timeline.insert(TweenLite.to(layer_2, 0.6, {css: {autoAlpha: 0.0}, ease: Power1.easeOut}), 2.26);
-timeline.insert(TweenLite.to(layer_2, 0.6, {css: {rotation: 720.0}, ease: Power1.easeOut}), 2.24);
-TweenLite.set(layer_2, {css: {autoAlpha: 0.0, left: 930, top: 284, scale: 0.6, rotation: 0.0}});
+// var layer_1 = document.getElementById("tui-9");
+// timeline.insert(TweenLite.to(layer_1, 0.74, {css: {top: 305, left: 382}, ease: Bounce.easeOut}), 3.6);//第二个时间是等待时间
+// timeline.insert(TweenLite.to(layer_1, 0.42, {css: {autoAlpha: 1.0}, ease: Power1.easeOut}), 3.6);
+// timeline.insert(TweenLite.to(layer_1, 1.74, {css: {top: -11, left: 23}, ease: Linear.easeNone}), 6.1);//移动
+// timeline.insert(TweenLite.to(layer_1, 1.74, {css: {scale: 0.6}, ease: Linear.easeNone}), 6.1);//变小
+// TweenLite.set(layer_1, {css: {autoAlpha: 0.0, left: 382, top: -84, scale: 1.0, rotation: 0.0}});//原本的状态
+// var layer_2 = document.getElementById("tui-8");
+// timeline.insert(TweenLite.to(layer_2, 0.72, {css: {top: 283, left: 643}, ease: Power4.easeOut}), 1.2);
+// timeline.insert(TweenLite.to(layer_2, 0.36, {css: {autoAlpha: 1.0}, ease: Power1.easeOut}), 1.2);
+// timeline.insert(TweenLite.to(layer_2, 0.44, {css: {scale: 1.84}, ease: Power4.easeOut}), 2.3);
+// timeline.insert(TweenLite.to(layer_2, 0.6, {css: {autoAlpha: 0.0}, ease: Power1.easeOut}), 2.26);
+// timeline.insert(TweenLite.to(layer_2, 0.6, {css: {rotation: 720.0}, ease: Power1.easeOut}), 2.24);
+// TweenLite.set(layer_2, {css: {autoAlpha: 0.0, left: 930, top: 284, scale: 0.6, rotation: 0.0}});
 var layer_3 = document.getElementById("tui-7");
 timeline.insert(TweenLite.to(layer_3, 1.08, {css: {scale: 1.7}, ease: Elastic.easeOut}), 0.24);
 timeline.insert(TweenLite.to(layer_3, 0.5, {css: {scale: 4.0}, ease: Power4.easeOut}), 2.3);
