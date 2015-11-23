3d73abcff30b2989c73beffa5d6579db428028a2
Lou Jiwei <jiwei.lou@foxmail.com>
2015-11-23 16:43:03 +0800

    y

--- javascript/TweenUI.js
+++ javascript/TweenUI.js
@@ -18,19 +18,19 @@ timeline.insert(TweenLite.to(layer_1, 0.42, {css: {autoAlpha: 1.0}, ease: Power1
 timeline.insert(TweenLite.to(layer_1, 1.74, {css: {top: -11, left: 23}, ease: Linear.easeNone}), 6.1);//移动
 timeline.insert(TweenLite.to(layer_1, 1.74, {css: {scale: 0.6}, ease: Linear.easeNone}), 6.1);//变小
 TweenLite.set(layer_1, {css: {autoAlpha: 0.0, left: 382, top: -84, scale: 1.0, rotation: 0.0}});//原本的状态
-var layer_2 = document.getElementById("tui-8");
-timeline.insert(TweenLite.to(layer_2, 0.72, {css: {top: 283, left: 643}, ease: Power4.easeOut}), 1.2);
-timeline.insert(TweenLite.to(layer_2, 0.36, {css: {autoAlpha: 1.0}, ease: Power1.easeOut}), 1.2);
-timeline.insert(TweenLite.to(layer_2, 0.44, {css: {scale: 1.84}, ease: Power4.easeOut}), 2.3);
-timeline.insert(TweenLite.to(layer_2, 0.6, {css: {autoAlpha: 0.0}, ease: Power1.easeOut}), 2.26);
-timeline.insert(TweenLite.to(layer_2, 0.6, {css: {rotation: 720.0}, ease: Power1.easeOut}), 2.24);
-TweenLite.set(layer_2, {css: {autoAlpha: 0.0, left: 930, top: 284, scale: 0.6, rotation: 0.0}});
-var layer_3 = document.getElementById("tui-7");
-timeline.insert(TweenLite.to(layer_3, 1.08, {css: {scale: 1.7}, ease: Elastic.easeOut}), 0.24);
-timeline.insert(TweenLite.to(layer_3, 0.5, {css: {scale: 4.0}, ease: Power4.easeOut}), 2.3);
-timeline.insert(TweenLite.to(layer_3, 0.6, {css: {autoAlpha: 0.0}, ease: Power1.easeOut}), 2.3);
-timeline.insert(TweenLite.to(layer_3, 0.56, {css: {rotation: 720.0}, ease: Power1.easeOut}), 2.3);
-TweenLite.set(layer_3, {css: {autoAlpha: 1.0, left: 492, top: 228, scale: 0.1, rotation: 0.0}});
+// var layer_2 = document.getElementById("tui-8");
+// timeline.insert(TweenLite.to(layer_2, 0.72, {css: {top: 283, left: 643}, ease: Power4.easeOut}), 1.2);
+// timeline.insert(TweenLite.to(layer_2, 0.36, {css: {autoAlpha: 1.0}, ease: Power1.easeOut}), 1.2);
+// timeline.insert(TweenLite.to(layer_2, 0.44, {css: {scale: 1.84}, ease: Power4.easeOut}), 2.3);
+// timeline.insert(TweenLite.to(layer_2, 0.6, {css: {autoAlpha: 0.0}, ease: Power1.easeOut}), 2.26);
+// timeline.insert(TweenLite.to(layer_2, 0.6, {css: {rotation: 720.0}, ease: Power1.easeOut}), 2.24);
+// TweenLite.set(layer_2, {css: {autoAlpha: 0.0, left: 930, top: 284, scale: 0.6, rotation: 0.0}});
+// var layer_3 = document.getElementById("tui-7");
+// timeline.insert(TweenLite.to(layer_3, 1.08, {css: {scale: 1.7}, ease: Elastic.easeOut}), 0.24);
+// timeline.insert(TweenLite.to(layer_3, 0.5, {css: {scale: 4.0}, ease: Power4.easeOut}), 2.3);
+// timeline.insert(TweenLite.to(layer_3, 0.6, {css: {autoAlpha: 0.0}, ease: Power1.easeOut}), 2.3);
+// timeline.insert(TweenLite.to(layer_3, 0.56, {css: {rotation: 720.0}, ease: Power1.easeOut}), 2.3);
+// TweenLite.set(layer_3, {css: {autoAlpha: 1.0, left: 492, top: 228, scale: 0.1, rotation: 0.0}});
 tui_go();
 }
 WebFontConfig = {google: { families: ['Lora', 'Permanent Marker'] }, active: function() { tui_go(); },inactive: function() { tui_go(); } };(function() {var wf = document.createElement('script');wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +'://ajax.googleapis.com/ajax/libs/webfont/1.0.31/webfont.js';wf.type = 'text/javascript';wf.async = 'true';var s = document.getElementsByTagName('script')[0];s.parentNode.insertBefore(wf, s);})();
